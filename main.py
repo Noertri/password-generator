1beddb5b02cd3cbbdc8f92fc9437fd1e238b4d77
Noertri <trinuruns@gmail.com>
2023-02-21 11:16:23 +0700

    validasi input

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import string
 def generate(length=6, numbers=False, spec_chars=False):
     letters = string.ascii_letters
     digits = string.digits
-    puncts = "!@#$%&-_"
+    puncts = "!@#$%&-_?"
 
     pwd = ""
 
@@ -31,8 +31,10 @@ def generate(length=6, numbers=False, spec_chars=False):
 if __name__ == "__main__":
     print("Program untuk membuat password acak secara otomatis")
     pwd_length = int(input("Panjang password: ").strip())
-    pwd_numbers = input("Tambahkan digit angka [y/n]: ").lower() == "y"
-    pwd_spec_chars = input("Tambahkan karakter khusus [y/n]: ").lower() == "y"
-    pwd = generate(length=pwd_length, spec_chars=pwd_numbers, numbers=pwd_spec_chars)
-    print("Password anda = ", pwd)
-    
+    if 6 < pwd_length <= 32:
+        pwd_numbers = input("Tambahkan digit angka [y/n]: ").lower() == "y"
+        pwd_spec_chars = input("Tambahkan karakter khusus [y/n]: ").lower() == "y"
+        pwd = generate(length=pwd_length, spec_chars=pwd_numbers, numbers=pwd_spec_chars)
+        print("Password anda = ", pwd)
+    else:
+        print("Panjang password minimal 6 dan maksimal 32!!!")
