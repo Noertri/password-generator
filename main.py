c6155b904a040ac14b9dc7b2028330294e125fb2
Noertri <trinuruns@gmail.com>
2023-02-23 11:42:06 +0700

    tes dan debug error

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ import os
 def generate(length=6, numbers=False, spec_chars=False):
     letters = string.ascii_letters
     digits = string.digits
-    puncts = "!@#$%&-_?"
+    puncts = "!@#$%&_?"
 
     pwd = ""
 
-    # menggunakan modul random choices untuk membut password
-    if numbers and not spec_chars:
-        letter = random.choices(letters+digits, k=length)
-    elif not numbers and spec_chars:
+    # menggunakan modul random choices untuk membuat password
+    if numbers and spec_chars:
+        letter = random.choices(letters+puncts+digits, k=length)
+    elif spec_chars:
         letter = random.choices(letters+puncts, k=length)
-    elif numbers and spec_chars:
-        letter = random.choices(letters+puncts+digits, k=length)     
+    elif numbers:
+        letter = random.choices(letters+digits, k=length)     
     else:
         letter = random.choices(letters, k=length)
 
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     if 6 <= pwd_length <= 32:
         pwd_numbers = input("Tambahkan digit angka [y/n]: ").lower() == "y"
         pwd_spec_chars = input("Tambahkan karakter khusus [y/n]: ").lower() == "y"
-        pwd = generate(length=pwd_length, spec_chars=pwd_numbers, numbers=pwd_spec_chars)
+        pwd = generate(length=pwd_length, numbers=pwd_numbers, spec_chars=pwd_spec_chars)
         print("Password anda: ", pwd)
     else:
         print("Panjang password minimal 6 dan maksimal 32!!!")
